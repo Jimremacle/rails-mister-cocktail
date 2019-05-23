cbbb3d92b8b7a73d358558ed9fc14a7e872c6b87
jeremy remacle <jeremy.remacle@gmail.com>
2019-05-23 16:02:41 +0200

    controllers done

--- app/models/dose.rb
+++ app/models/dose.rb
@@ -1,2 +1,6 @@
 class Dose < ApplicationRecord
+  belongs_to :ingredient
+  belongs_to :cocktail
+  validates :description, presence: true, allow_blank: false
+  validates_uniqueness_of :ingredient_id, :dose => [:cocktail_id]
 end
