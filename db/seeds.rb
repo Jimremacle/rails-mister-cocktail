e1884d20c09eea5e12adc6faab81899ebfa23d61
jeremy remacle <jeremy.remacle@gmail.com>
2019-05-24 12:04:06 +0200

    trying heroku

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,14 +1,17 @@
-# require 'open-uri'
-# require 'json'
-Ingredient.create(name: 'lemon')
-Ingredient.create(name: 'ice')
-Ingredient.create(name: 'mint leaves')
+require 'open-uri'
+require 'json'
+# Ingredient.create(name: 'lemon')
+# Ingredient.create(name: 'ice')
+# Ingredient.create(name: 'mint leaves')
 
-# url = "http://www.thecocktaildb.com/api/json/v1/1/list.php?i=list"
-# url_serialized = open(url).read
-# ingredients = JSON.parse(url_serialized)
+Ingredient.destroy_all
+puts 'ingredients destroyed'
+puts 'creating ingredients'
+url = 'https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list'
 
-# for num in [10, 11, 12]
-#   p "--" + name = ingredients["drinks"][num]["strIngredient1"]
-#   Ingredient.create(name: name)
-# end
+results = JSON.parse(open(url).read)
+ingredients = results['drinks']
+ingredients.each do |ingredient|
+  Ingredient.create(name: ingredient['strIngredient1'])
+end
+puts 'done'
